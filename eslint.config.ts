41e76e8102367517034b8fc9446f0eaa15a925a2
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-23 00:47:20 +0530

    Added eslint and it's dependencies

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["src/**/*.ts"],
+        extends: [js.configs.recommended, tseslint.configs.recommended],
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+]);
