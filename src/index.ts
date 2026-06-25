f1c8af9e5b13b657002b722d2653da1191d3a17a
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-06-25 22:38:59 +0530

    Echoed inputs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("My first github action!!");
+import * as core from "@actions/core";
+// import * as github from "@actions/github";
+
+async function main() {
+    const manifestFilePathname = core.getInput("manifest-file");
+    const leetcodeSessionCookie = core.getInput("leetcode-session-cookie");
+
+    console.log("manifestFilePathname:", manifestFilePathname);
+    console.log("leetcodeSessionCookie:", leetcodeSessionCookie);
+}
+
+main();
